6bb9505dc27d42c337187d07a7d86c9bab4e26cc
Xander Smeets <xander@smeets.ee>
2023-08-11 12:42:54 +0200

    Create symlinks in correct directories & provide config templates

--- entrypoint.server.sh
+++ entrypoint.server.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 cd /home/container
 
+# Create config files & directories
+if [ ! -e /home/container/server/config/avatars ]; then
+  cp -r /opt/pokemon-showdown/server/config.template/avatars /home/container/server/config
+fi
+if [ ! -e /home/container/server/config/chat-plugins ]; then
+  cp -r /opt/pokemon-showdown/server/config.template/chat-plugins /home/container/server/config
+fi
+if [ ! -e /home/container/server/config/ladders ]; then
+  cp -r /opt/pokemon-showdown/server/config.template/ladders /home/container/server/config
+fi
+if [ ! -e /home/container/server/config/hosts.csv ]; then
+  cp -r /opt/pokemon-showdown/server/config.template/hosts.csv /home/container/server/config
+fi
+if [ ! -e /home/container/server/config/proxies.csv ]; then
+  cp -r /opt/pokemon-showdown/server/config.template/proxies.csv /home/container/server/config
+fi
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
